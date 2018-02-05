29add79841989cd1fc680018b7bcb8ed7a72cea7
Chris Davis <contact@nthstate.com>
2018-02-05 21:50:51 +0000

    Move tests to js

--- test/TestContractCreation.js
+++ test/TestContractCreation.js
@@ -1,4 +1,3 @@
-
 var Keepwrite = artifacts.require("Keepwrite");
 
 contract('Keepwrite', function(accounts) {
@@ -9,23 +8,5 @@ contract('Keepwrite', function(accounts) {
     	assert.equal(owner, accounts[0], "Owner should match");
     });
   });
-  
-  it("should have version 1", function() {
-    return Keepwrite.deployed().then(function(instance) {
-    	return instance.getVersion.call();
-    }).then(function(version) {
-    	assert.equal(version, 1, "Version 1");
-    });
-  });
-  
-});
 
-// contract('Keepwrite', function(accounts) {
-//   it("should create a contract and the owner is the creator account", function() {
-//     return MetaCoin.deployed().then(function(instance) {
-//       return instance.getBalance.call(accounts[0]);
-//     }).then(function(balance) {
-//       assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-//     });
-//   });
-// });
+});
